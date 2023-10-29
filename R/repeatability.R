00ba842c1b8ba8983573f79eaad0106243192d41
“Jonah <warren.jonahb@tamu.edu>
2023-10-29 06:30:25 -0400

    Added roxygen skeleton.

--- R/repeatability.R
+++ R/repeatability.R
@@ -1,3 +1,11 @@
+#' Title
+#'
+#' @param meas Measured value for the feature.
+#'
+#' @return Xbar repeatability calculation.
+#' @export
+#'
+#' @examples
 repeat_calc = function(meas){
    xbar = mean(means)
    R = max(meas) - min(meas)
