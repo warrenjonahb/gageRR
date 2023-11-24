700651cd4b9df7d8237653e3269131d3af02fb68
“Jonah <warren.jonahb@tamu.edu>
2023-11-24 14:23:54 -0500

    Added dplry summarize info. Updated all range calculation (incorrectly used range previously)

--- R/xbar_r_varcomps.R
+++ R/xbar_r_varcomps.R
@@ -1,7 +1,7 @@
 #TODO
 #Source data.R script for g/m vals
 source("data.R")
-
+options(dplyr.summarise.inform = FALSE)
 xbar_repeat = function(data, part, operator, meas){
   reps = data %>%
     select({{part}}, {{operator}}) %>%
@@ -13,6 +13,8 @@ xbar_repeat = function(data, part, operator, meas){
     stop("Each part must have an equal number of replicates")
   }
 
+  reps = unique(reps$rep)
+
   a = data %>%
     select({{part}}) %>%
     distinct() %>%
@@ -23,18 +25,19 @@ xbar_repeat = function(data, part, operator, meas){
     distinct() %>%
     count()
 
-  g1 = a*k
+  g1 = as.integer(a*k)
 
   d = d_table %>%
     filter(g == g1 & m == reps) %>%
-    select(d)
+    select(d2) %>%
+    as.double()
 
   xbar_rep = data %>%
     select({{part}}, {{operator}}, {{meas}}) %>%
     group_by({{part}}, {{operator}}) %>%
-    summarize(repeatbility = range({{meas}})/(a*k)*1/d)
+    summarize(repeatbility = (max({{meas}})-min({{meas}}))/(a*k))
 
-  repeatability = sum(xbar_rep$repeatbility)
+  repeatability = (sum(xbar_rep$repeatbility)/d)^2 #Squaring to return varComp not SD
 
   return(repeatability)
 
@@ -57,28 +60,34 @@ xbar_reproduce = function(data, part, operator, meas){
   a = data %>%
     select({{part}}) %>%
     distinct() %>%
-    count()
+    count() %>%
+    as.integer()
 
   m1 = data %>%
     select({{operator}}) %>%
     distinct() %>%
-    count()
+    count() %>%
+    as.integer()
 
-  #need to data mask?
   d = d_table %>%
-    filter(g == 1 & m == m1)
+    filter(g == 1 & m == m1) %>%
+    select(d2) %>%
+    as.double()
 
   xbar_i = data %>%
     select({{operator}}, {{meas}}) %>%
     group_by({{operator}}) %>%
-    summarize(op_range = range({{meas}}))
+    summarize(op_avg = mean({{meas}}))
 
-  x_diff = max(xbar_i$op_range) - min(xbar_i$op_range)
+  x_diff = max(xbar_i$op_avg) - min(xbar_i$op_avg)
 
-  repeatability = xbar_repeat(data = data, part = part, operator = operator, meas = meas)
+  repeatability = xbar_repeat(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
 
-  reproducibility = sqrt((x_diff *1/d)^2 - (repeatabilty^2/(a*r)))
-
+  if((x_diff *1/d)^2 - (repeatability^2/(a*r)) > 0) {
+    reproducibility = (x_diff *1/d)^2 - (repeatability^2/(a*r))
+  }else{
+    reproducibility = 0
+  }
   return(reproducibility)
 
 }
@@ -86,14 +95,22 @@ xbar_reproduce = function(data, part, operator, meas){
 #Need to pass inputs to ss_calcs and get back g and m.
 part_to_part = function(data, part, meas){
 
+  a = data %>%
+    select({{part}}) %>%
+    distinct() %>%
+    count() %>%
+    as.integer()
+
+  d = d_table %>%
+    filter(g == 1 & m == a) %>%
+    select(d2) %>%
+    as.double()
+
   part_meas = data %>%
     group_by({{part}}) %>%
     summarize(avg_meas = mean({{meas}}))
 
-  d = d_table %>%
-    filter(g == 1 & m == m)
-
-  r_p = range(part_meas$avg_meas)/d
+  r_p = ((max(part_meas$avg_meas) - min(part_meas$avg_meas))/d)^2
 
   return(r_p)
 
@@ -104,14 +121,14 @@ xbar_varcomps = function(data, part, operator = NULL, meas)  {
   repeatability = xbar_repeat(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
   reproducibility = xbar_reproduce(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
   part_to_part = part_to_part(data = {{data}}, part = {{part}}, meas = {{meas}})
-  total_grr = repeatabiity + reproducibility
+  total_grr = repeatability + reproducibility
   total_var = total_grr + part_to_part
 
   return(list(
-    repeatability,
-    reproducibility,
-    total_grr,
-    part_to_part,
-    total_var
+    repeatability = repeatability,
+    reproducibility = reproducibility,
+    total_grr = total_grr,
+    part_to_part = part_to_part,
+    total_var = total_var
   ))
 }
