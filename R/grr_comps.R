60bc9d0b667b37993714583466fc6a96c1f0dd7f
“Jonah <warren.jonahb@tamu.edu>
2023-11-24 19:50:49 -0500

    Updated grr_comps to return dataframe of VarComps for xbar_r and anova methods.

--- R/grr_comps.R
+++ R/grr_comps.R
@@ -6,18 +6,17 @@ source("xbar_r_vacromps.R")
 
 grr_calc = function(data, part, operator = NULL, meas, method = 'anova') {
   if(method == 'anova') {
-
     varComps = anova_var_calcs(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
     gage_eval = data.frame(matrix(unlist(varComps)))
     row.names(gage_eval) <-names(varComps)
-
-
+    colnames(gage_eval) <- 'VarComp'
   }
 
   if(method == 'xbar_r') {
-
-    xbar_varcomps(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
-
+    varComps = xbar_varcomps(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
+    gage_eval = data.frame(matrix(unlist(varComps)))
+    row.names(gage_eval) <-names(varComps)
+    colnames(gage_eval) <- 'VarComp'
     }
-
+return(gage_eval)
 }
