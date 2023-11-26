b7a312263a876b219ba932942f9a6b27b43ae6f3
“Jonah <warren.jonahb@tamu.edu>
2023-11-26 07:18:13 -0500

    Created GageEval table for StudyVar

--- R/grr_comps.R
+++ R/grr_comps.R
@@ -1,22 +1,26 @@
-#TO DO
-#GRR comps
-#Based on user selection "xbar_R" or "ANOVA"
+
 source("anova_varcomps.R")
 source("xbar_r_vacromps.R")
 
 grr_calc = function(data, part, operator = NULL, meas, method = 'anova') {
+
   if(method == 'anova') {
     varComps = anova_var_calcs(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
-    gage_eval = data.frame(matrix(unlist(varComps)))
-    row.names(gage_eval) <-names(varComps)
-    colnames(gage_eval) <- 'VarComp'
-  }
-
-  if(method == 'xbar_r') {
+  }else{
     varComps = xbar_varcomps(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
-    gage_eval = data.frame(matrix(unlist(varComps)))
-    row.names(gage_eval) <-names(varComps)
-    colnames(gage_eval) <- 'VarComp'
-    }
-return(gage_eval)
+  }
+    VarianceComponents = data.frame(matrix(unlist(varComps)))
+    row.names(VarianceComponents) <-names(varComps)
+    colnames(VarianceComponents) <- 'VarComp'
+
+    TotalVariation = VarianceComponents['total_var',]
+    VarianceComponents['PercentContribution'] = VarianceComponents['VarComp']/TotalVariation
+
+    GageEval = data.frame(row.names = rownames(VarianceComponents))
+    GageEval['StdDev'] = sqrt(VarianceComponents$VarComp)
+    GageEval['StudyVar'] =  GageEval * 6
+    TotalStudyVar = GageEval['total_var', 'StudyVar']
+    GageEval['PercentStudyVar'] = GageEval['StudyVar'] / TotalStudyVar
+
+return(list(VarianceComponents = VarianceComponents, GageEval = GageEval))
 }
