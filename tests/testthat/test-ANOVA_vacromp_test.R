89e38c532b4fa60a5f20962faa5eb43cc1b033fd
“Jonah <warren.jonahb@tamu.edu>
2023-12-03 14:34:20 -0500

    Added testing to ANOVA VARCOMP and XBAR VARCOMP

--- tests/testthat/test-ANOVA_vacromp_test.R
+++ tests/testthat/test-ANOVA_vacromp_test.R
@@ -1,3 +1,44 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+  data = data.frame(
+    SN = c(
+      'SerialNumber_01',
+      'SerialNumber_01',
+      'SerialNumber_01',
+      'SerialNumber_02',
+      'SerialNumber_02',
+      'SerialNumber_02'),
+
+    Operator = c(
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01'),
+
+    Measure = c(
+      1,
+      1,
+      1,
+      3,
+      3,
+      3))
+
+  singleOperTest_SS = ss_calcs(data, part = SN, operator = Operator, meas = Measure)
+  expect_equal(singleOperTest_SS$reps, 3)
+  expect_equal(singleOperTest_SS$num_parts, 2)
+  expect_equal(singleOperTest_SS$num_opers, 1)
+  expect_equal(singleOperTest_SS$SS_oper_error, 0)
+  expect_equal(singleOperTest_SS$SS_part_error, 6)
+  expect_equal(singleOperTest_SS$SS_op_part_error, 0)
+  expect_equal(singleOperTest_SS$SS_total_error, 6)
+
+  singleOperTest_anova = anova_var_calcs(data, part = SN, operator = Operator, meas = Measure)
+  expect_equal(singleOperTest_anova$repeatability,0)
+  expect_equal(singleOperTest_anova$reproducibility,0)
+  expect_equal(singleOperTest_anova$total_grr,0)
+  expect_equal(singleOperTest_anova$part_to_part,2)
+  expect_equal(singleOperTest_anova$total_var,2)
+
 })
--- tests/testthat/test-xbar_vacromp_test.R
+++ tests/testthat/test-xbar_vacromp_test.R
@@ -1,3 +1,37 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  data = data.frame(
+    SN = c(
+      'SerialNumber_01',
+      'SerialNumber_01',
+      'SerialNumber_01',
+      'SerialNumber_02',
+      'SerialNumber_02',
+      'SerialNumber_02'),
+
+    Operator = c(
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01'),
+
+    Measure = c(
+      1,
+      1,
+      1,
+      3,
+      3,
+      3))
+
+  expect_equal(xbar_repeat(data, part = SN, operator = Operator, meas = Measure), 0)
+  expect_equal(xbar_reproduce(data, part = SN, operator = Operator, meas = Measure), 0)
+  expect_equal(round(part_to_part(data, part = SN, meas = Measure),3),2)
+
+  xbar_varcompTest = xbar_varcomps(data, part = SN, operator = Operator, meas = Measure)
+  expect_equal(xbar_varcompTest$repeatability,0)
+  expect_equal(xbar_varcompTest$reproducibility,0)
+  expect_equal(xbar_varcompTest$total_grr,0)
+  expect_equal(round(xbar_varcompTest$part_to_part,3),2)
+  expect_equal(round(xbar_varcompTest$total_var,3),2)
 })
