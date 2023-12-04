64dc692781e4da2505e34303cb8da1d4de806ae1
“Jonah <warren.jonahb@tamu.edu>
2023-12-04 18:46:32 -0500

    Added tests for grr_comp

--- tests/testthat/test-grr_comp_test.R
+++ tests/testthat/test-grr_comp_test.R
@@ -1,3 +1,63 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  data = data.frame(
+    SN = c(
+      'SerialNumber_01',
+      'SerialNumber_01',
+      'SerialNumber_02',
+      'SerialNumber_02',
+      'SerialNumber_01',
+      'SerialNumber_01',
+      'SerialNumber_02',
+      'SerialNumber_02'),
+
+    Operator = c(
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_01',
+      'Operator_02',
+      'Operator_02',
+      'Operator_02',
+      'Operator_02'),
+
+    Measure = c(
+      0.0172,
+      0.0177,
+      0.0155,
+      0.0159,
+      0.0174,
+      0.0181,
+      0.0152,
+      0.0176))
+
+  grr_anova_check = grr_calc(data, part = SN, operator = Operator, meas = Measure, LSL = .150, USL = .180, method = 'anova')
+
+  expect_equal(round(grr_anova_check$VarianceComponents["repeatability", "PercentContribution"],3), .393)
+  expect_equal(round(grr_anova_check$VarianceComponents["reproducibility", "PercentContribution"],3), .050)
+  expect_equal(round(grr_anova_check$VarianceComponents["total_grr", "PercentContribution"],3), .442)
+  expect_equal(round(grr_anova_check$VarianceComponents["part_to_part", "PercentContribution"],3), .558)
+  expect_equal(round(grr_anova_check$VarianceComponents["total_var", "PercentContribution"],3), 1.000)
+
+  expect_equal(round(grr_anova_check$GageEval["repeatability", "PercentTolerance"],3), .182)
+  expect_equal(round(grr_anova_check$GageEval["reproducibility", "PercentTolerance"],3), .065)
+  expect_equal(round(grr_anova_check$GageEval["total_grr", "PercentTolerance"],3), .194)
+  expect_equal(round(grr_anova_check$GageEval["part_to_part", "PercentTolerance"],3), .217)
+  expect_equal(round(grr_anova_check$GageEval["total_var", "PercentTolerance"],3), .291)
+
+  grr_xbar_check = grr_calc(data, part = SN, operator = Operator, meas = Measure, LSL = .150, USL = .180, method = 'xbar_r')
+
+  expect_equal(round(grr_xbar_check$VarianceComponents["repeatability", "PercentContribution"],3), .341)
+  expect_equal(round(grr_xbar_check$VarianceComponents["reproducibility", "PercentContribution"],3), .062)
+  expect_equal(round(grr_xbar_check$VarianceComponents["total_grr", "PercentContribution"],3), .403)
+  expect_equal(round(grr_xbar_check$VarianceComponents["part_to_part", "PercentContribution"],3), .597)
+  expect_equal(round(grr_xbar_check$VarianceComponents["total_var", "PercentContribution"],3), 1.000)
+
+  expect_equal(round(grr_xbar_check$GageEval["repeatability", "PercentTolerance"],3), .166)
+  expect_equal(round(grr_xbar_check$GageEval["reproducibility", "PercentTolerance"],3), .071)
+  expect_equal(round(grr_xbar_check$GageEval["total_grr", "PercentTolerance"],3), .180)
+  expect_equal(round(grr_xbar_check$GageEval["part_to_part", "PercentTolerance"],3), .219)
+  expect_equal(round(grr_xbar_check$GageEval["total_var", "PercentTolerance"],3), .284)
+
+  expect_error(grr_calc(data, part = SN, operator = Operator, meas = Measure, LSL = .150, USL = NULL, method = 'anova'))
+  expect_error(grr_calc(data, part = SN, operator = Operator, meas = Measure, LSL = .150, USL = NULL, method = 'avg_range'))
 })
