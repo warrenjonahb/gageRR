d6b95ad54b04b010d8640fc82a2e520859e7234b
Jonah Warren <jonah.warren@geaerospace.com>
2025-09-09 18:15:36 -0400

    Implicit call to stats::pf()

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Measurement System Analysis Data
+#'
+#' A collection of sample data to analyze for reproducibility and repeatability measurement error.
+#'
+#' @format ## `data`
+#' A data frame with 27 rows and 3 columns:
+#' \describe{
+#'   \item{SN}{Serial Number}
+#'   \item{Operator}{Operator or Measurement Technician}
+#'   \item{Measure}{Measured Value}
+#'   ...
+#' }
+"data"
