9f4b4e2a1b0529b7aba4bdb779df86d2d63c5e45
warrenjonahb <warren.jonahb@gmail.com>
2025-09-15 06:32:31 -0400

    Changed syntax to require strings for column names.

--- R/anova_varcomps.R
+++ R/anova_varcomps.R
@@ -40,70 +40,59 @@
 #' 0.0152,
 #' 0.0176))
 #'
-#'ss_calcs(data, part = SN, operator = Operator, meas = Measure)
+#'ss_calcs(data, part = 'SN', operator = 'Operator', meas = 'Measure')
 
-ss_calcs = function(data, part, operator, meas){
+ss_calcs <- function(data, part, operator, meas) {
+  # count reps per part/operator
+  reps <- aggregate(data[[meas]],
+                    by = list(data[[part]], data[[operator]]),
+                    FUN = length)
 
-    reps = data %>%
-      dplyr::select({{part}}, {{operator}}) %>%
-      dplyr::group_by({{part}}, {{operator}}) %>%
-      dplyr::summarize(rep = n(), .groups = 'keep')
+  if (length(unique(reps$x)) != 1) {
+    stop("Each part must have an equal number of replicates")
+  }
+  r <- unique(reps$x)
 
-  #Small correction here need equal number of reps across each operator/part combo
-    if (length(unique(reps$rep)) != 1) {
-      stop("Each part must have an equal number of replicates")
-    }
+  p <- length(unique(data[[part]]))
+  t <- length(unique(data[[operator]]))
 
-    r = unique(reps$rep)
+  overall_mean <- mean(data[[meas]])
 
-    p = data %>%
-        dplyr::select({{part}}) %>%
-        distinct() %>%
-        count()
+  # Operator SS
+  op_means <- tapply(data[[meas]], data[[operator]], mean)
+  SS_oper_error <- sum((op_means - overall_mean)^2) * p * r
 
-    t = data %>%
-      dplyr::select({{operator}}) %>%
-      distinct() %>%
-      count()
+  # Part SS
+  part_means <- tapply(data[[meas]], data[[part]], mean)
+  SS_part_error <- sum((part_means - overall_mean)^2) * t * r
 
-    SS_oper = data %>%
-      dplyr::mutate(overall_mean = mean({{meas}})) %>%
-      dplyr::group_by({{operator}}) %>%
-      dplyr::mutate(op_mean = mean({{meas}})) %>%
-      dplyr::mutate(sq_error = (op_mean - overall_mean)^2)
+  # Total SS
+  SS_total_error <- sum((data[[meas]] - overall_mean)^2)
 
-    SS_oper_error = sum(SS_oper$sq_error)
+  # Equipment (repeatability)
+  op_part_means <- aggregate(data[[meas]],
+                             by = list(data[[operator]], data[[part]]),
+                             FUN = mean)
+  merged <- merge(data, op_part_means,
+                  by.x = c(operator, part),
+                  by.y = c("Group.1", "Group.2"))
+  SS_equip_error <- sum((merged$x - merged[[meas]])^2) # deviation within op*part
 
-    SS_part = data %>%
-      dplyr::mutate(overall_mean = mean({{meas}})) %>%
-      dplyr::group_by({{part}}) %>%
-      dplyr::mutate(part_mean = mean({{meas}})) %>%
-      dplyr::mutate(sq_error = (part_mean -overall_mean)^2)
+  # Operator*Part interaction
+  SS_op_part_error <- SS_total_error - (SS_oper_error + SS_part_error + SS_equip_error)
 
-    SS_part_error = sum(SS_part$sq_error)
-
-    SS_total = data %>%
-      dplyr::mutate(overall_mean = mean({{meas}})) %>%
-      dplyr::mutate(sq_error = ({{meas}} - overall_mean)^2)
-
-    SS_total_error = sum(SS_total$sq_error)
-
-    SS_equip = data %>%
-      dplyr::group_by({{operator}}, {{part}}) %>%
-      dplyr::mutate(op_part_mean = mean({{meas}})) %>%
-      dplyr::mutate(sq_error = (op_part_mean - {{meas}})^2)
-
-    SS_equip_error = sum(SS_equip$sq_error)
-
-    SS_op_part_error = SS_total_error - (SS_oper_error + SS_part_error + SS_equip_error)
-
-    return(list(reps = as.integer(r), num_parts = as.integer(p), num_opers = as.integer(t),
-                SS_oper_error = as.double(SS_oper_error),
-                SS_part_error = as.double(SS_part_error),
-                SS_equip_error = as.double(SS_equip_error),
-                SS_op_part_error = as.double(SS_op_part_error),
-                SS_total_error = as.double(SS_total_error)))
+  return(list(
+    reps = as.integer(r),
+    num_parts = as.integer(p),
+    num_opers = as.integer(t),
+    SS_oper_error = as.double(SS_oper_error),
+    SS_part_error = as.double(SS_part_error),
+    SS_equip_error = as.double(SS_equip_error),
+    SS_op_part_error = as.double(SS_op_part_error),
+    SS_total_error = as.double(SS_total_error)
+  ))
 }
+
 
 #' ANOVA Variance Component Calculations
 #'
@@ -147,88 +136,65 @@ ss_calcs = function(data, part, operator, meas){
 #' 0.0152,
 #' 0.0176))
 #'
-#'anova_var_calcs(data, part = SN, operator = Operator, meas = Measure)
+#'anova_var_calcs(data, part = 'SN', operator = 'Operator', meas = 'Measure')
 
-anova_var_calcs = function(data, part, operator, meas)  {
+anova_var_calcs <- function(data, part, operator, meas) {
+  ss_comp <- ss_calcs(data, part, operator, meas)
 
-  ss_comp = ss_calcs(data = {{data}}, part = {{part}}, operator = {{operator}}, meas = {{meas}})
+  reps <- ss_comp$reps
+  num_parts <- ss_comp$num_parts
+  num_opers <- ss_comp$num_opers
+  SS_oper_error <- ss_comp$SS_oper_error
+  SS_part_error <- ss_comp$SS_part_error
+  SS_equip_error <- ss_comp$SS_equip_error
+  SS_op_part_error <- ss_comp$SS_op_part_error
+  SS_total_error <- ss_comp$SS_total_error
 
-  reps = ss_comp$reps
-  num_parts = ss_comp$num_parts
-  num_opers = ss_comp$num_opers
-  SS_oper_error = ss_comp$SS_oper_error
-  SS_part_error = ss_comp$SS_part_error
-  SS_equip_error = ss_comp$SS_equip_error
-  SS_op_part_error = ss_comp$SS_op_part_error
-  SS_total_error = ss_comp$SS_total_error
+  MS_oper <- if (num_opers == 1) 0 else SS_oper_error / (num_opers - 1)
+  MS_part <- if (num_parts == 1) 0 else SS_part_error / (num_parts - 1)
+  MS_oper_part <- if (num_parts == 1 | num_opers == 1) 0 else SS_op_part_error / ((num_opers - 1) * (num_parts - 1))
+  MS_equip <- SS_equip_error / (num_parts * num_opers * (reps - 1))
 
-  if(num_opers == 1){
-    MS_oper = 0
-  }else{
-    MS_oper = SS_oper_error/(num_opers - 1)
+  # Compute p-value for interaction
+  if (num_parts == 1 | num_opers == 1) {
+    p_val <- NULL
+  } else {
+    F_stat <- MS_oper_part / MS_equip
+    p_val <- stats::pf(F_stat,
+                       df1 = as.integer((num_opers - 1) * (num_parts - 1)),
+                       df2 = as.integer(num_parts * num_opers * (reps - 1)),
+                       lower.tail = FALSE)
   }
 
-  if(num_parts == 1){
-    MS_part = 0
-  }else{
-    MS_part = SS_part_error/(num_parts - 1)
+  if (!is.null(p_val) && p_val < .05) {
+    MS_equip <- (SS_equip_error + SS_op_part_error) /
+      (((num_opers - 1) * (num_parts - 1)) + (num_parts * num_opers * (reps - 1)))
   }
 
-  if(num_parts == 1 | num_opers == 1){
-    MS_oper_part = 0
-  }else{
-    MS_oper_part = SS_op_part_error/((num_opers - 1)*(num_parts - 1))
+  var_repeat <- max(MS_equip, 0)
+  var_oper_part <- max((MS_part - MS_equip) / reps, 0)
+  var_part <- max((MS_part - MS_oper_part) / (reps * num_opers), 0)
+  var_oper <- max((MS_oper - MS_oper_part) / (reps * num_parts), 0)
+
+  if (is.null(p_val) || p_val > .05) {
+    var_oper_part <- 0
   }
 
-  MS_equip = SS_equip_error / (num_parts * num_opers * (reps-1))
+  repeatability <- var_repeat
+  reproducibility <- var_oper + var_oper_part
+  total_grr <- repeatability + reproducibility
+  part_to_part <- var_part
+  total_var <- total_grr + part_to_part
 
-  #compute p-val part_oper interaction
-  if(num_parts == 1 | num_opers == 1){
-    p_val = NULL
-  }else {
-
-  F_stat = MS_oper_part/(MS_equip)
-  p_val = stats::pf(F_stat[[1]],
-             df1 = as.integer((num_opers - 1)*(num_parts - 1)),
-             df2 = as.integer(num_parts * num_opers * (reps-1)),
-            lower.tail = FALSE    )}
-
-  if(is.null(p_val)){
-    MS_equip = SS_equip_error / (num_parts * num_opers * (reps-1))
-  }else  if (p_val < .05) {
-    MS_equip = (SS_equip_error + SS_op_part_error)/((num_opers - 1)*(num_parts - 1)+(num_parts * num_opers * (reps-1)))
-  }else{
-    MS_equip = SS_equip_error / (num_parts * num_opers * (reps-1))
-    }
-
-  var_repeat = MS_equip
-  var_oper_part = (MS_part - MS_equip)/reps
-  var_part = (MS_part - MS_oper_part)/(reps * num_opers)
-  var_oper = (MS_oper - MS_oper_part)/(reps * num_parts)
-
-  if (is.null(p_val)) {
-    var_oper_part = 0
-  }else  if (p_val > .05) {
-    var_oper_part = 0
-  }
-
-  if(var_repeat<0){var_repeat=0}
-  if(var_oper_part<0){var_oper_part=0}
-  if(var_part<0){var_part=0}
-  if(var_oper<0){var_oper=0}
-
-  repeatability = var_repeat
-  reproducibility = var_oper + var_oper_part
-  total_grr = repeatability + reproducibility
-  part_to_part = var_part
-  total_var = total_grr + part_to_part
-
-  return(list(total_grr = as.double(total_grr),
-              repeatability = as.double(repeatability),
-              reproducibility = as.double(reproducibility),
-              part_to_part = as.double(part_to_part),
-              total_var = as.double(total_var)))
+  return(list(
+    total_grr = total_grr,
+    repeatability = repeatability,
+    reproducibility = reproducibility,
+    part_to_part = part_to_part,
+    total_var = total_var
+  ))
 }
+
 
 #' ANOVA Table Calculation
 #'
@@ -272,22 +238,14 @@ anova_var_calcs = function(data, part, operator, meas)  {
 #' 0.0152,
 #' 0.0176))
 #'
-#'anova_table(data, part = SN, operator = Operator, meas = Measure)
+#'anova_table(data, part = 'SN', operator = 'Operator', meas = 'Measure')
 
-anova_table = function(data, part, operator, meas)  {
-  part_chr     <- gsub("[{}]", "", deparse(substitute(part)))
-  operator_chr <- gsub("[{}]", "", deparse(substitute(operator)))
-  meas_chr     <- gsub("[{}]", "", deparse(substitute(meas)))
-
-  # Build the formula string
-  form_text <- sprintf("%s ~ %s * %s", meas_chr, operator_chr, part_chr)
-
-  # Create formula
+anova_table <- function(data, part, operator, meas) {
+  # build formula from strings
+  form_text <- sprintf("%s ~ %s * %s", meas, operator, part)
   form <- as.formula(form_text, env = parent.frame())
 
-  # Run ANOVA
+  # run ANOVA
   anova_stats <- aov(form, data = data)
-  anova_table = summary(anova_stats)
-
-  return(anova_table)
-  }
+  summary(anova_stats)
+}
