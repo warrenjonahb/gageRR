5709b3d187be2686f5b59a4e232af8994c05192a
warrenjonahb <warren.jonahb@gmail.com>
2025-09-27 16:28:32 -0400

    Update SS calcs to include SS_no_interaction

--- R/anova_varcomps.R
+++ R/anova_varcomps.R
@@ -182,19 +182,24 @@ anova_var_calcs <- function(data, part, operator, meas) {
   }
 
   if (!is.null(p_val) && p_val < .05) {
-    MS_equip <- (SS_equip_error ) /
-      (num_parts * num_opers * (reps - 1))
+    MS_equip <- (SS_equip_error ) / (num_parts * num_opers * (reps - 1))
+
+    var_oper <- max((MS_oper - MS_oper_part) / (reps * num_parts), 0)
   }
 
   if (is.null(p_val) || p_val > .05) {
     MS_equip <- (SS_equip_error + SS_op_part_error) /
       (((num_opers - 1) * (num_parts - 1)) + (num_parts * num_opers * (reps - 1)))
+
+    var_oper <- max((SS_op_part_error + SS_equip_error) /
+                      ((num_opers-1)*(num_parts-1)+num_opers*num_parts*(reps-1)), 0)
+
   }
 
   var_repeat <- max(MS_equip, 0)
   var_oper_part <- max((MS_part - MS_equip) / reps, 0)
   var_part <- max((MS_part - MS_oper_part) / (reps * num_opers), 0)
-  var_oper <- max((MS_oper - MS_oper_part) / (reps * num_parts), 0)
+
 
   repeatability <- var_repeat
   reproducibility <- var_oper + var_oper_part
